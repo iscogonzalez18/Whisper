87dbf724e1bb5479f21a967a3b8259de2d41ccf1
iscogonzalez18 <83782270+iscogonzalez18@users.noreply.github.com>
2023-01-12 14:15:45 -0500

    response done, curl not yet

--- chat.py
+++ chat.py
@@ -1,8 +1,9 @@
 import openai
 import os
 
+print("Loading OpenAI GPT...")
 openai.organization = "org-d6sOkmC5UFSQEz57mmi0kpzM"
-openai.api_key = os.getenv("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")
+openai.api_key = "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"
 
 def chat(prompt):
     response = openai.Completion.create(
@@ -20,3 +21,11 @@ def chat(prompt):
 prompt = "Human: Hello, who are you?"
 
 print(chat(prompt))
+
+# request
+
+curl -X POST https://api.openai.com/v1/engines/davinci/completions \
+    -H 'Content-Type: application/json' \
+    
+
+print("Done!")
